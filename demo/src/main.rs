0a2396fefb050878de66fc47301000a980445e38
Bill Fraser <wfraser@codewise.org>
2018-05-08 03:54:01 -0700

    move most stuff out of lib.rs and into details.rs

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,14 +1,14 @@
 extern crate wasm_binary;
 
-use wasm_binary::Read;
-use std::fs::File;
-
 fn main() {
-    let mut f = File::open(
+    let mut f = std::fs::File::open(
         std::env::args_os().nth(1)
             .expect("need a file to open"))
         .expect("file open error");
 
-    let module = wasm_binary::Module::read(&mut f).unwrap();
+    let module = wasm_binary::Module::read(&mut f).unwrap_or_else(|(e, offset)| {
+        eprintln!("Error at {:#x}: {:?}", offset, e);
+        std::process::exit(1);
+    });
     println!("{:#?}", module);
 }
